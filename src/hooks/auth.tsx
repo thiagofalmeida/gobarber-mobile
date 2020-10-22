72d4532505df6e07668145b98b9afd04eae12589
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2020-10-21 22:48:38 -0300

    Lista de prestadores

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -14,15 +14,22 @@ interface SignInCredencials {
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   loading: boolean;
   signIn(credentials: SignInCredencials): Promise<void>;
   signOut(): void;
 }
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
+}
+
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
@@ -39,6 +46,8 @@ const AuthProvider: React.FC = ({ children }) => {
       ]);
 
       if (user[1] && token[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
+
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
 
@@ -60,6 +69,8 @@ const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:token', token],
       ['@GoBarber:user', JSON.stringify(user)]
     ]);
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
